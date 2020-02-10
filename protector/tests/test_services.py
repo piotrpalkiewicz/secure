3df035f948a0ba39450dc2584ecfeeb68587d122
Piotr Palkiewicz <ppalk@softserveinc.com>
2020-02-10 10:24:16 +0100

    Reformat code

--- protector/tests/test_services.py
+++ protector/tests/test_services.py
@@ -8,15 +8,18 @@ from protector.services import is_password_match, is_valid_link
 
 
 class ServicesUnitTestCase(unittest.TestCase):
-
     @mock.patch.object(Resource, "objects")
-    def test_is_password_match_should_return_true_when_password_match(self, resource_mock):
+    def test_is_password_match_should_return_true_when_password_match(
+        self, resource_mock
+    ):
         resource_mock.get.return_value = mock.Mock()
 
         self.assertTrue(is_password_match(protected_url="", password=""))
 
     @mock.patch.object(Resource, "objects")
-    def test_is_password_match_should_return_false_when_obj_not_found(self, resource_mock):
+    def test_is_password_match_should_return_false_when_obj_not_found(
+        self, resource_mock
+    ):
         resource_mock.get.side_effect = mock.Mock(side_effect=Resource.DoesNotExist)
 
         self.assertFalse(is_password_match(protected_url="", password=""))
